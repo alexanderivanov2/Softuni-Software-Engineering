c37daf52dc46e961e8ec40bce14cd01c1889e759
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-02-23 16:45:49 +0200

    Lab Exercises

---
+++ Python-Programing-Basics/First Steps in Coding/First Steps in Coding - Lab/03_square_area/square_area.py
@@ -0,0 +1,9 @@
+a = int(input())
+
+
+def square_area(n):
+    area = n * n
+    return area
+
+
+print(square_area(a))
---
+++ Python-Programing-Basics/First Steps in Coding/First Steps in Coding - Lab/04_inches_to_centimeters/inch_to_cm.py
@@ -0,0 +1,9 @@
+inches = float(input())
+
+
+def convert_to_cm(n):
+    cm = n * 2.54
+    return cm
+
+
+print(convert_to_cm(inches))
