ef8183ed6d6f6662b6b6e206793b9df0f723e5f7
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-03-26 00:22:50 +0200

    Lab

---
+++ Python-Programing-Basics/Conditional Statements Advanced/Conditional Statements Advanced - Lab/08_cinema_ticket/cinema_tickets.py
@@ -0,0 +1,8 @@
+day = input()
+
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+price = [12, 12, 14, 14, 12, 16, 16]
+
+index = days.index(day)
+
+print(price[index])
