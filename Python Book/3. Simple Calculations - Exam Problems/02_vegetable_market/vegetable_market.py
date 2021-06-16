49ae5980cb78a8147f7d457ba7634fa83e004119
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-06-16 23:15:34 +0300

    Exercise

---
+++ Python Book/3. Simple Calculations - Exam Problems/02_vegetable_market/vegetable_market.py
@@ -0,0 +1,9 @@
+# Console Entry
+N = float(input())
+M = float(input())
+N_kg = int(input())
+M_kg = int(input())
+# Formula
+income = ((N * N_kg) + (M * M_kg)) / 1.94
+# Console Exit
+print(income)
