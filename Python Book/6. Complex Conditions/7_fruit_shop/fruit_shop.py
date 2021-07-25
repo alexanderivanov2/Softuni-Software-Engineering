fc9bf378c20fa402df58986c149a96ada3e2848f
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-07-25 23:19:01 +0300

    Exercise

---
+++ Python Book/6. Complex Conditions/7_fruit_shop/fruit_shop.py
@@ -0,0 +1,57 @@
+fruit = input().lower()
+day_of_week = input().title()
+quantity = float(input())
+
+if (day_of_week == "Monday" or day_of_week == "Tuesday" or day_of_week == "Wednesday" or
+        day_of_week == "Thursday" or day_of_week == "Friday"):
+    if fruit == "banana":
+        price = quantity * 2.50
+        print(f"{price:.2f}")
+    elif fruit == "apple":
+        price = quantity * 1.20
+        print(f"{price:.2f}")
+    elif fruit == "orange":
+        price = quantity * 0.85
+        print(f"{price:.2f}")
+    elif fruit == "grapefruit":
+        price = quantity * 1.45
+        print(f"{price:.2f}")
+    elif fruit == "kiwi":
+        price = quantity * 2.70
+        print(f"{price:.2f}")
+    elif fruit == "pineapple":
+        price = quantity * 5.50
+        print(f"{price:.2f}")
+    elif fruit == "grapes":
+        price = quantity * 3.85
+        print(f"{price:.2f}")
+    else:
+        print("error")
+
+elif day_of_week == "Saturday" or day_of_week == "Sunday":
+    if fruit == "banana":
+        price = quantity * 2.70
+        print(f"{price:.2f}")
+    elif fruit == "apple":
+        price = quantity * 1.25
+        print(f"{price:.2f}")
+    elif fruit == "orange":
+        price = quantity * 0.90
+        print(f"{price:.2f}")
+    elif fruit == "grapefruit":
+        price = quantity * 1.60
+        print(f"{price:.2f}")
+    elif fruit == "kiwi":
+        price = quantity * 3.00
+        print(f"{price:.2f}")
+    elif fruit == "pineapple":
+        price = quantity * 5.60
+        print(f"{price:.2f}")
+    elif fruit == "grapes":
+        price = quantity * 4.20
+        print(f"{price:.2f}")
+    else:
+        print("error")    
+
+else:
+    print("error")  
