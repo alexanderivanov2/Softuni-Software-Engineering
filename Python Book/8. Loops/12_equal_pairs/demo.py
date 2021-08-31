87ba75181351dbf06085aacf2ce154c79bd33bb0
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-08-31 23:29:46 +0300

    Exercise

--- Python Book/8. Loops/12_equal_pairs/demo.py
+++ Python Book/8. Loops/12_equal_pairs/demo.py
@@ -3,3 +3,17 @@ previsious_sum = 0
 current_sum = 0
 diff = 0
 maxdiff = 0
+
+for i in range(n):
+    num1 = int(input())
+    num2 = int(input())
+    curent_sum = num1 + num2
+    diff = abs(curent_sum - previsious_sum)
+    previsious_sum = curent_sum
+
+if n <= 1:
+    print(f"Yes, value = {diff}")
+elif diff == 0:
+    print(f"Yes, value = {previsious_sum}")
+else:
+    print(f"No, maxdiff = {diff}")
