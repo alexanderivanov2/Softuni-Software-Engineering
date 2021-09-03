611a129465898ff10e93b068f96d1df870ec78d2
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-09-03 17:02:59 +0300

    Exercise

--- Python Book/9. Loops - Exam Problems/02_smart_lilly/demo.py
+++ Python Book/9. Loops - Exam Problems/02_smart_lilly/demo.py
@@ -4,3 +4,17 @@ toys_price = int(input())
 saved_money = 0
 toy_number = 0
 money_for_birthday = 10
+
+for i in range(1, years + 1):
+    if i % 2 == 0:
+        saved_money += (money_for_birthday -1)
+        money_for_birthday += 10
+    else:
+        toy_number += 1
+
+saved_money += (toy_number * toys_price)
+
+if saved_money >= price_wash_machine:
+    print(f"Yes! {saved_money - price_wash_machine:.2f}")
+else:
+    print(f"No! {price_wash_machine - saved_money:.2f}")
