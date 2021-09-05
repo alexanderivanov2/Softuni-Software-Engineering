57d6432c3fea1063e6a7ef8954c1089d1cf64f1e
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-09-05 23:19:45 +0300

    Exercise

--- Python Book/9. Loops - Exam Problems/03_back_to_the_past/demo.py
+++ Python Book/9. Loops - Exam Problems/03_back_to_the_past/demo.py
@@ -6,3 +6,13 @@ spent_money_odd = 0
 
 for i in range(1800, end_year + 1):
     years += 1
+    if i % 2 == 0:
+        spent_money_even += 12000
+    else:
+        spent_money_odd += 12000 + 50 * years
+
+    if spent_money_even + spent_money_odd <= heritage:
+        print(f"Yes! He will live a carefree life and will have {heritage - (spent_money_even + spent_money_odd):.2f} "
+              f"dollars left.")
+    else:
+        print(f"He will need {(spent_money_odd + spent_money_even) - heritage:.2f} dollars to survive.")
