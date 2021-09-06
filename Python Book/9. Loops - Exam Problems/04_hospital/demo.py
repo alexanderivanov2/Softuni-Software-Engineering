ac74a13df448af892cb4896e6264b1f0dedf91eb
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-09-06 21:59:58 +0300

    Exercise

--- Python Book/9. Loops - Exam Problems/04_hospital/demo.py
+++ Python Book/9. Loops - Exam Problems/04_hospital/demo.py
@@ -5,3 +5,16 @@ untrated = 0
 
 for i in range(1, days + 1):
     patients = int(input())
+    if i % 3 == 0 and untrated > treated:
+        doctors += 1
+
+    if patients == doctors:
+        treated += doctors
+    elif doctors > patients:
+        treated += patients
+    else:
+        treated += doctors
+        untrated += patients - doctors
+
+print(f"Treated patients: {treated}.")
+print(f"Untreated patients: {untrated}.")
