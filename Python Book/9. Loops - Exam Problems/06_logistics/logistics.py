90043ba461ac61fb1f678ce662c74212b1f2f74c
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-09-09 23:35:59 +0300

    Exercise

--- Python Book/9. Loops - Exam Problems/06_logistics/logistics.py
+++ Python Book/9. Loops - Exam Problems/06_logistics/logistics.py
@@ -5,3 +5,15 @@ p3 = 0
 
 for i in range(n):
     num = int(input())
+    if num % 2 == 0:
+        p1 += 1
+
+    if num % 3 == 0:
+        p2 += 1
+
+    if num % 4 == 0:
+        p3 += 1
+
+print(f"{(p1 / n) * 100:.2f}%")
+print(f"{(p2 / n) * 100:.2f}%")
+print(f"{(p3 / n) * 100:.2f}%")
