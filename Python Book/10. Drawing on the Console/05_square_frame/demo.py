0350524320ecab206fff4af75a84545e41bdd57e
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-09-16 23:57:48 +0300

    Exercise

--- Python Book/10. Drawing on the Console/05_square_frame/demo.py
+++ Python Book/10. Drawing on the Console/05_square_frame/demo.py
@@ -3,3 +3,13 @@ n = int(input())
 print("+", end=" ")
 print(("-" + " ") * (n - 2), end="")
 print("+")
+
+for cow in range(n-2):
+    print("|", end=" ")
+    print(("-" + " ") * (n - 2), end="")
+    print("|", end=" ")
+    print()
+
+print("+", end=" ")
+print(("-" + " ") * (n - 2), end="")
+print("+")
