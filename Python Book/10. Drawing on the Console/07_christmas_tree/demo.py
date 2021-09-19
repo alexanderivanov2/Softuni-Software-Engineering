a7f5cbc689108bb3b4d0475bdacdec25f83c9386
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-09-19 23:21:59 +0300

    Exercise

--- Python Book/10. Drawing on the Console/07_christmas_tree/demo.py
+++ Python Book/10. Drawing on the Console/07_christmas_tree/demo.py
@@ -4,3 +4,8 @@ print(" " * (n + 1) + "|")
 for row in range(1, n + 1):
     space = n - row
     stars = row
+    print(" " * space, end="")
+    print("*" * stars, end="")
+    print(" " + "|", end=" ")
+    print("*" * stars, end="")
+    print(" ")
