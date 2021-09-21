96cebe2ab704c84582ec34f42f14d89288f773f9
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-09-21 23:12:40 +0300

    Exercise

--- Python Book/10. Drawing on the Console/08_sunglasses/demo.py
+++ Python Book/10. Drawing on the Console/08_sunglasses/demo.py
@@ -10,3 +10,18 @@ middle4 = "|" * n
 print(up_down, end="")
 print(middle2, end="")
 print(up_down)
+
+for row in range(1, n - 2 + 1):
+    print("*" + middle + "*", end="")
+    if n == (n % 2 == 0) and row == 0:
+        print(middle4, end="")
+    elif row != middle_row:
+        print(middle2, end="")
+    else:
+        print(middle4, end="")
+    print("*" + middle + "*")
+
+
+print(up_down, end="")
+print(middle2, end="")
+print(up_down, end="")
