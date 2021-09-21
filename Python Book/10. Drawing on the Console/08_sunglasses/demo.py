6c7569603dbe8b089c56e0133d5086120b5ca2f8
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-09-21 23:12:14 +0300

    Exercise

--- Python Book/10. Drawing on the Console/08_sunglasses/demo.py
+++ Python Book/10. Drawing on the Console/08_sunglasses/demo.py
@@ -1,1 +1,12 @@
+import math
 
+n = int(input())
+middle_row = math.ceil((n - 2) / 2)
+up_down = "*" * (n * 2)
+middle = "/" * (n + (n - 2))
+middle2 = " " * n
+middle4 = "|" * n
+
+print(up_down, end="")
+print(middle2, end="")
+print(up_down)
