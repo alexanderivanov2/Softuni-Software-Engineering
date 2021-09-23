8088a88850c0dda011a92f703ca49545ff1376f4
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-09-23 23:04:34 +0300

    Exercise

--- Python Book/10. Drawing on the Console/09_house/demo.py
+++ Python Book/10. Drawing on the Console/09_house/demo.py
@@ -8,3 +8,20 @@ sides = "|"
 roof = math.ceil(n / 2)
 floors = n - roof
 
+print(down_line * (roof - 1), end="")
+if n % 2 == 0:
+    print(stars * 2, end="")
+else:
+    print(stars, end="")
+print(down_line * (roof - 1))
+
+if not n % 2 == 0:
+    for middle_of_the_roof_odd in range(2, roof):
+        print(down_line * (roof - middle_of_the_roof_odd), end="")
+        print(stars * (middle_of_the_roof_odd * 2 - 1), end="")
+        print(down_line * (roof - middle_of_the_roof_odd))
+else:
+    for middle_of_the_roof_even in range(2, roof):
+        print(down_line * (roof - middle_of_the_roof_even), end="")
+        print(stars * (middle_of_the_roof_even * 2), end="")
+        print(down_line * (roof - middle_of_the_roof_even))
