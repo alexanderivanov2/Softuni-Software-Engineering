a8c6bc94137ec4e2952ed377462f946717b9b3ee
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-09-25 23:12:08 +0300

    Exercise

--- Python Book/10. Drawing on the Console/10_diamond/demo.py
+++ Python Book/10. Drawing on the Console/10_diamond/demo.py
@@ -26,3 +26,27 @@ for top_rows in range(2, rows + 1):
     print(star, end="")
     print(line * (n1 - (top_rows + rows)))
     middle_line += 2
+
+if n < 3:
+    print("", end="")
+else:
+    print(star + (line * (n - 2)) + star)
+
+for down_rows in range(1, rows):
+    if n < 3:
+        print("", end="")
+    else:
+        print(line * down_rows, end="")
+        print(star + (line * (n - 4)) + star, end="")
+        print(line * down_rows)
+        n -= 2
+
+if n < 3:
+    print("",end="")
+else:
+    print(line * rows, end="")
+    if n % 2 == 0:
+        print(star * 2, end="")
+    else:
+        print(star, end="")
+print(line * rows)
