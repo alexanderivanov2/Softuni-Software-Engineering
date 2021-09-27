8b6426349ccf84c9ae02908d17897e958c0adb51
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-09-28 00:35:46 +0300

    Exercise

--- Python Book/11. Drawing on the Console - Exam Problems/02_butterfly/demo.py
+++ Python Book/11. Drawing on the Console - Exam Problems/02_butterfly/demo.py
@@ -4,3 +4,11 @@ width = 2 * n - 1
 height = 2 * (n - 2) + 1
 left_or_right_part_width = n - 1
 left, right, star, dash, center, space = "\\", "/", "*", "-", "@", " "
+
+for up_half in range(1, (height - 1) // 2 + 1):
+    if not up_half % 2 == 0:
+        print(star * (left_or_right_part_width - 1) + left + space + right + star * (left_or_right_part_width - 1))
+    else:
+        print(dash * (left_or_right_part_width - 1) + left + space + right + dash * (left_or_right_part_width - 1))
+
+print(space * left_or_right_part_width + center + space * left_or_right_part_width)
