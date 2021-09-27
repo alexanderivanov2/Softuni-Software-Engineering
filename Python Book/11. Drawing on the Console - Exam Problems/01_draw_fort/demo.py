aad24119ac637623c10fc13d063c7c47b6957c80
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-09-27 23:50:29 +0300

    Exercise

--- Python Book/11. Drawing on the Console - Exam Problems/01_draw_fort/demo.py
+++ Python Book/11. Drawing on the Console - Exam Problems/01_draw_fort/demo.py
@@ -10,3 +10,12 @@ between_left_right = n - (n // 2 + 2)
 
 print(right_line + up_middle * (n // 2) + left_line + down_line * (between_left_right * 2) + right_line + up_middle
       * (n // 2) + left_line)
+
+for row in range(1, n - 1):
+    if row == n - 1 - 1:
+        print(side + space * (n // 2 + 1) + down_line * (between_left_right * 2) + space * (n // 2 + 1) + side)
+    else:
+        print(side + space * (n * 2 - 2) + side)
+
+print(left_line + down_line * (n // 2) + right_line + space * (between_left_right * 2) + left_line + down_line *
+      (n // 2) + right_line)
