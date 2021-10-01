abf944f788e4a02101ea6f1e496fd74515b3ac29
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-10-01 23:28:59 +0300

    Exercise

--- Python Book/11. Drawing on the Console - Exam Problems/03_stop/demo.py
+++ Python Book/11. Drawing on the Console - Exam Problems/03_stop/demo.py
@@ -8,3 +8,10 @@ middle_downline_up_to_down = top_downline - 2 # + 2 to middle, - 2 after middle
 left_right_side_middle = (middle_downline_up_to_down - 7 + (top_points * 2)) // 2
 downline_after_middle = (middle_downline_up_to_down - 2 + (top_points * 2))
 down_after_mid = 2
+
+print(points * top_points + downline * top_downline + points * top_points)
+for i in range(1, top_points):
+    print(points * (top_points - 1) + right_line + downline * middle_downline_up_to_down + left_line * 2
+          + points * (top_points - 1))
+    top_points -= 1
+    middle_downline_up_to_down += 2
