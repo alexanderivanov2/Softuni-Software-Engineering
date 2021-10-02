d0775c2a336841ada79a1d2b095df30a469bb025
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-10-02 21:33:25 +0300

    Exercise

--- Python Book/11. Drawing on the Console - Exam Problems/04_arrow/demo.py
+++ Python Book/11. Drawing on the Console - Exam Problems/04_arrow/demo.py
@@ -6,3 +6,11 @@ before_mid_inside_points = n - 2
 mid_grid = before_mid_outside_points + 1
 points_after_mid = 0
 width = before_mid_outside_points * 2 + n
+
+print(points * before_mid_outside_points + grid * n + points * before_mid_outside_points)
+
+for row in range(1, n - 1):
+    print(points * before_mid_outside_points + grid + points * before_mid_inside_points + grid
+          + points * before_mid_outside_points)
+
+print(grid * mid_grid + points * before_mid_inside_points + grid * mid_grid)
