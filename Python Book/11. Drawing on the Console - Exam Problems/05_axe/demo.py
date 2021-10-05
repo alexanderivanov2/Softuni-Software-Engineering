524d3e616661a67b823b4b4f9595b816176a441e
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-10-05 23:23:11 +0300

    Exercise

---
+++ Python Book/11. Drawing on the Console - Exam Problems/05_axe/demo.py
@@ -0,0 +1,14 @@
+n = int(input())
+down_line, stars = "-", "*"
+width = 5 * n
+width_down_line_left = n * 3
+width_down_line_right = width - width_down_line_left - 2  # - row
+height_top_to_middle = n
+mid_and_end = n // 2
+end_of_axe = height_top_to_middle + 1 + ((height_top_to_middle - 1) // 2)
+end_down_line = (width_down_line_left - ((end_of_axe - height_top_to_middle - 1) // 2))
+after_mid_downline = (height_top_to_middle - 1)
+end_right_downline = (width - width_down_line_left - after_mid_downline)
+print(down_line * width_down_line_left + stars * 2 + down_line * width_down_line_right)
+for row in range(1, height_top_to_middle):
+    print(down_line * width_down_line_left + stars + down_line * row + stars + down_line * (width_down_line_right - row))
