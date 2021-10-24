d945e8d68763dab36654a9171234e919f30a1db4
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-10-24 19:04:15 +0300

    Exercise

--- Python Book/12.  Complex Loops/10_check_prime/demo.py
+++ Python Book/12.  Complex Loops/10_check_prime/demo.py
@@ -1,3 +1,13 @@
 import math
 n = int(input())
 prime = True
+
+for i in range(2, int((math.sqrt(abs(n))) + 1)):
+    if n % i == 0:
+        prime = False
+        break
+
+if prime and n > 1:
+    print("prime")
+else:
+    print("Not prime")
