f954c72bc418b28c9a8c9a85c1416f25b5168aa0
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-11-08 23:08:09 +0200

    Exercise

--- Python Book/13. Complex Loops - Exam Problems/01_stupid_password_generator/demo.py
+++ Python Book/13. Complex Loops - Exam Problems/01_stupid_password_generator/demo.py
@@ -6,3 +6,15 @@ end_num = 1
 sym1 = ord("a")
 sym2 = ord("a")
 last_char = sym1 + l
+
+while not (num1 == n == num2 and sym1 + 2 == last_char == sym2 + 2):
+    while not end_num == num_end:
+        end_num += 1
+        print(f"{num1}{num2}{chr(sym1)}{chr(sym2)}{end_num}", end=" ")
+    if sym1 != last_char:
+        sym2 += 1
+        if sym2 == last_char:
+            sym1 += 1
+            if l == 1 and num1 == n == num2 and end_num == num_end:
+                break
+            sym2 = ord("a")
