31fdcc4ea7543627cda7c749d9983b8ca5ef9c87
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-11-12 00:03:18 +0200

    Exercise

--- Python Book/13. Complex Loops - Exam Problems/03_stop_number/demo.py
+++ Python Book/13. Complex Loops - Exam Problems/03_stop_number/demo.py
@@ -1,3 +1,10 @@
 N = int(input())
 M = int(input())
 S = int(input())
+
+
+for num in range(M, N - 1, - 1):
+    if num % 2 == 0 and num % 3 == 0:
+        if num == S:
+            break
+        print(num, end=" ")
