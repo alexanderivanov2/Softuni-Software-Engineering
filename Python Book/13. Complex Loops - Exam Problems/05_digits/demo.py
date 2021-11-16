b77c11ba500151585e190feb20d216e8681d27a0
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-11-16 23:53:17 +0200

    Exercise

--- Python Book/13. Complex Loops - Exam Problems/05_digits/demo.py
+++ Python Book/13. Complex Loops - Exam Problems/05_digits/demo.py
@@ -7,3 +7,14 @@ n3 = n % 10
 end_num = n
 rows = n1 + n2
 number_on_rows = n1 + n3
+
+for row in range(1, rows + 1):
+    for num_on_rows in range(1, number_on_rows + 1):
+        if end_num % 5 == 0:
+            end_num = end_num - n1
+        elif end_num % 3 == 0:
+            end_num = end_num - n2
+        else:
+            end_num = end_num + n3
+        print(end_num, end=" ")
+    print()
