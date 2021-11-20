287466fd23acfdc37da4d08a516c45c2442899fa
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-11-20 22:49:56 +0200

    Exercise

--- Python Book/14. Exam Preparation - Part 1/03_point_on_segment/demo.py
+++ Python Book/14. Exam Preparation - Part 1/03_point_on_segment/demo.py
@@ -4,3 +4,14 @@ point = int(input())
 
 left = min(begin, ending)
 right = max(begin, ending)
+
+distance_left = abs(left - point)
+distance_right = abs(right - point)
+min_distance = min(distance_left, distance_right)
+
+if left <= point <= right:
+    print("in")
+else:
+    print("out")
+
+print(min_distance)
