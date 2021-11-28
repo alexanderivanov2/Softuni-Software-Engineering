36ab76f41e9f18cf86a6c391676b70815ad7d2a2
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-11-28 23:42:06 +0200

    Exercise

--- Python Book/14. Exam Preparation - Part 1/point_in_figure/demo.py
+++ Python Book/14. Exam Preparation - Part 1/point_in_figure/demo.py
@@ -2,3 +2,8 @@ x = int(input())
 y = int(input())
 
 x1, y1, x2, y2, x3, y3, x4, y4 = 4, 3, 10, -5, 2, 1, 12, -3
+
+if x1 <= x <= x2 and y1 >= y >= y2 or (x3 <= x <= x4 and y3 >= y >= y4):
+    print("in")
+else:
+    print("out")
