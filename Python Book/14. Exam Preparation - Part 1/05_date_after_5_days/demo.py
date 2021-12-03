97d68a777ecead7ea6338265788560ee565b2b40
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-12-03 20:24:33 +0200

    Exercise

--- Python Book/14. Exam Preparation - Part 1/05_date_after_5_days/demo.py
+++ Python Book/14. Exam Preparation - Part 1/05_date_after_5_days/demo.py
@@ -7,3 +7,17 @@ if m == 2:
     days_in_month = 28
 elif m == 4 or m == 6 or m == 9 or m == 11:
     days_in_month = 30
+
+if d + 5 > days_in_month:
+    d = (d + 5) - days_in_month
+    m += 1
+    if m > 12:
+        m = 1
+else:
+    d += 5
+
+
+if m < 10:
+    print(f"{d}.0{m}")
+else:
+    print(f"{d}.{m}")
