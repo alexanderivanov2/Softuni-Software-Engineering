420bc4417d7fbe2af2ed3a272667ed6a0f8a5a49
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-12-12 15:09:07 +0200

    Exercise

---
+++ Full Java Basics/SquareOf7Multiply7Stars.java
@@ -0,0 +1,7 @@
+public class SquareOf7Multiply7Stars {
+    public static void main(String[] arg){
+        String TxtLine = "* * * * * * *";
+        for (int i=1; i <= 7; i++)
+            System.out.println(TxtLine);
+    }
+}
