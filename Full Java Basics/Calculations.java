bb49730aedecfb5cd111492d25eb9257bd21afe8
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-12-12 15:05:17 +0200

    Exercise

---
+++ Full Java Basics/Calculations.java
@@ -0,0 +1,7 @@
+public class Calculations {
+    public static void main(String[] arg){
+        System.out.println(5 + 3 * 2);
+        System.out.println(4 * (2 + 3));
+        System.out.println((2+5) * (8-2)/ 7);
+    }
+}
