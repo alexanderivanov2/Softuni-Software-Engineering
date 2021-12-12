24b1b8f07ba1d4f5e6257b42919cd0c7e6db212e
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-12-12 14:54:04 +0200

    Exercise

---
+++ Full Java Basics/NameAndExpression.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class NameAndExpression {
+    public static void main(String[] arg){
+        Scanner scan = new Scanner(System.in);
+        String name = scan.next();
+        System.out.println(name);
+        System.out.println(5+3);
+    }
+}
