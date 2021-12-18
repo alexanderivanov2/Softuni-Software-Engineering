62aa8cff0295f9f4db4f36726c343f8887f2c022
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-12-18 23:37:56 +0200

    Exercise

--- Python Book/15. Exam Preparation - Part 2/christmas_hat/demo.py
+++ Python Book/15. Exam Preparation - Part 2/christmas_hat/demo.py
@@ -9,3 +9,14 @@ rows = height - 6
 print(point * begin_points + "/|\\" + point * begin_points)
 print(point * begin_points + "\\|/" + point * begin_points)
 print(point * begin_points + star * 3 + point * begin_points)
+
+for row in range(1, rows + 1):
+    print(point * (begin_points - row) + star + line * row + star + line * row + star + point * (begin_points - row))
+
+print(star * width)
+print(star, end="")
+
+for i in range(1, n * 2 + 1):
+    print(point + star, end="")
+print()
+print(star * width)
