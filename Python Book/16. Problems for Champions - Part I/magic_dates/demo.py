aee2fd63b5838c8b26cb8bfe3d7d79e36bf5ee95
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-12-23 23:23:20 +0200

    Exercise

--- Python Book/16. Problems for Champions - Part I/magic_dates/demo.py
+++ Python Book/16. Problems for Champions - Part I/magic_dates/demo.py
@@ -45,3 +45,14 @@ for year in range(start_year, end_year + 1):
             else:
                 d2 = d % 10
                 d1 = (d // 10) % 10
+
+            formula1 = d1 * d2 + d1 * m1 + d1 * m2 + d1 * y1 + d1 * y2 + d1 * y3 + d1 * y4 + d2 * m1 + d2 * m2 + d2 * y1\
+                       + d2 * y2 + d2 * y3 + d2 * y4
+            formula2 = m1 * m2 + m1 * y1 + m1 * y2 + m1 * y3 + m1 * y4 + m2 * y1 + m2 * y2 + m2 * y3 + m2 * y4 + y1 * y2\
+                       + y1 * y3 + y1 * y4 + y2 * y3 + y2 * y4 + y3 * y4
+            if formula1 + formula2 == date_weight:
+                print(str(d1) + str(d2) + "-" + str(m1) + str(m2) + "-" + str(y1) + str(y2) + str(y3) + str(y4))
+                have = False
+
+if have:
+    print("No")
