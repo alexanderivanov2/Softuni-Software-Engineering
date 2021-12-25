736c43df470e9640b780dc566cdefac3c390d167
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-12-26 00:40:02 +0200

    Exercise

---
+++ Python Book/17. Problems for Champions - Part II/passion_days/demo.py
@@ -0,0 +1,12 @@
+from decimal import Decimal as Dec
+
+money_have = Dec(input())
+word = input()
+purchace = 0
+total = money_have
+expenses = 0
+
+while word != "mall.Enter":
+    word = input()
+
+word = input()
