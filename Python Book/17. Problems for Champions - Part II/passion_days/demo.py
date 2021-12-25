95760c35d1af45f6c45d8aed9cf2bff266b6ce4c
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-12-26 00:40:42 +0200

    Exercise

--- Python Book/17. Problems for Champions - Part II/passion_days/demo.py
+++ Python Book/17. Problems for Champions - Part II/passion_days/demo.py
@@ -10,3 +10,29 @@ while word != "mall.Enter":
     word = input()
 
 word = input()
+
+while word != "mall.Exit":
+    for digit in word:
+        chr1 = ord(digit)
+
+        if 65 <= chr1 <= 90:
+            expenses = Dec(chr1 * 0.5)
+        elif 97 <= chr1 <= 122:
+            expenses = Dec(chr1 * 0.3)
+        elif chr1 == 37:
+            expenses = Dec(total / 2)
+        elif chr1 == 42:
+            total = total + 10
+        else:
+            expenses = Dec(chr1)
+
+        if expenses <= total and expenses != 0:
+            total = Dec(total - expenses)
+            purchace += 1
+        expenses = 0
+    word = input()
+
+if purchace > 0:
+    print(f"{purchace} purchases. Money left: {total:.2f} lv.")
+else:
+    print(f"No purchases. Money left: {total:.2f} lv.")
