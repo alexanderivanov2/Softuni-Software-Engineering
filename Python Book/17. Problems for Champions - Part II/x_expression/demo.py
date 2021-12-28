c3cfe457a971992fe320bfbba2b11578fe244503
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2021-12-28 20:27:19 +0200

    Exercise

--- Python Book/17. Problems for Champions - Part II/x_expression/demo.py
+++ Python Book/17. Problems for Champions - Part II/x_expression/demo.py
@@ -6,3 +6,29 @@ result = 0
 expression_operator = "+"
 inner_operator = "+"
 inner_result = 0
+
+while symbol != "=":
+    if symbol == "(":
+        inner_result = 0
+        inner_operator = "+"
+        while symbol != ")":
+            symbol = symbols.pop(0)
+            if 49 <= ord(symbol) <= 57:
+                if inner_operator == "+":
+                    inner_result += ord(symbol) - ord("0")
+                elif inner_operator == "-":
+                    inner_result -= ord(symbol) - ord("0")
+                elif inner_operator == "/":
+                    inner_result /= ord(symbol) - ord("0")
+                elif inner_operator == "*":
+                    inner_result *= ord(symbol) - ord("0")
+            elif symbol == "+" or symbol == "-" or symbol == "/" or symbol == "*":
+                inner_operator = symbol
+        if expression_operator == "+":
+            result += inner_result
+        elif expression_operator == "-":
+            result -= inner_result
+        elif expression_operator == "/":
+            result /= inner_result
+        elif expression_operator == "*":
+            result *= inner_result
