ee0f3af4b78ab0ed20441b1d2028da4cfb5f0b16
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-01-01 22:42:17 +0200

    Exercise

---
+++ Python Book/18. Functions/blank_receipt/demo.py
@@ -0,0 +1,22 @@
+def print_header_top():
+    print("CASH RECEIPT")
+    print("------------------------------")
+
+
+def print_middle_part():
+    print("Charged to____________________")
+    print("Received by___________________")
+
+
+def print_bottom_part():
+    print("------------------------------")
+    print("(c) SoftUni")
+
+
+def print_all_parts():
+    print_header_top()
+    print_middle_part()
+    print_bottom_part()
+
+
+print_all_parts()
