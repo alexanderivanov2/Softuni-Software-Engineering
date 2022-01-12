c3b5d364a52ee350f2148696b81f4e7dfba55ac9
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-01-12 23:19:01 +0200

    Exercise

---
+++ Full Java Basics/Conditional-Statements-Advanced-Java/GreatestNumberOutOfThree.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class GreatestNumberOutOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a > b){
+            if (a > c)
+                System.out.println(a);
+            else
+                System.out.println(c);
+        } else{
+            if (b > c)
+                System.out.println(b);
+            else
+                System.out.println(c);
+        }
+    }
+}
