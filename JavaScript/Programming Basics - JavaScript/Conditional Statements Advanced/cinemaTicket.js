7524b9e4e82f01c2252dae1279e552d50732d906
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-02-25 22:51:20 +0200

    Exercise

---
+++ JavaScript/Programming Basics - JavaScript/Conditional Statements Advanced/cinemaTicket.js
@@ -0,0 +1,10 @@
+function getPriceOfTicket (input) {
+    let day = input[0];
+    if (day === 'Monday' || day === 'Tuesday' || day === 'Friday') {
+        console.log(12);
+    } else if(day === 'Wednesday' || day === 'Thursday') {
+        console.log(14);
+    } else {
+        console.log(16);
+    }
+}
