283d09977251185018b56b6278b740d4c1f1c5d0
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-02-25 22:51:33 +0200

    Exercise

---
+++ JavaScript/Programming Basics - JavaScript/Conditional Statements Advanced/dayOfWeek.js
@@ -0,0 +1,13 @@
+function getDayOfTheWeek (input) {
+    let numForDay = input[0];
+    switch (numForDay){
+        case '1': console.log('Monday'); break;
+        case '2': console.log('Tuesday'); break;
+        case '3': console.log('Wednesday'); break;
+        case '4': console.log('Thursday'); break;
+        case '5': console.log('Friday'); break;
+        case '6': console.log('Saturday'); break;
+        case '7': console.log('Sunday'); break;
+        default: console.log('Error'); break;
+    }
+}
