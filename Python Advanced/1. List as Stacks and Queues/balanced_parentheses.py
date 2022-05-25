609f6297f0cf8bc92d7ffef8751b2fcf6e651ef0
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-05-25 14:15:02 +0300

    Exercise

--- Python Advanced/1. List as Stacks and Queues/balanced_parentheses.py
+++ Python Advanced/1. List as Stacks and Queues/balanced_parentheses.py
@@ -1,27 +1,33 @@
-opening = ["(", "[", "{"]
-close = [")", "]", "}"]
+from collections import deque
 
-line = input()
-stack = []
-for el in line:
-    if el in opening:
-        stack.append(el)
-    elif el in close and len(stack) == 0:
-        print("NO")
-        exit()
-    else:
-        last = stack.pop()
-        if last == "{" and el == "}":
-            continue
-        elif last == "[" and el == "]":
-            continue
-        elif last == "(" and el == ")":
-            continue
-        else:
-            print("NO")
-            exit()
+expression = deque([el for el in input()])
 
-if stack:
-    print("NO")
+opened = ["[", "{", "("]
+closed = ["]", "}", ")"]
+
+opened_stack = []
+balanced = True
+
+if len(expression) % 2 == 0:
+    while expression:
+        parentheses = expression.popleft()
+        if parentheses in opened:
+            opened_stack.append(parentheses)
+        elif parentheses in closed:
+            open_parentheses = opened_stack.pop()
+            if parentheses == ")" and open_parentheses == "(":
+                continue
+            elif parentheses == "]" and open_parentheses == "[":
+                continue
+            elif parentheses == "}" and open_parentheses == "{":
+                continue
+            else:
+                balanced = False
+                break
 else:
+    balanced = False
+
+if balanced:
     print("YES")
+else:
+    print("NO")
