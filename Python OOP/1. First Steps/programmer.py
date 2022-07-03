9e73877238d49a68429cb1a79577c5af22fd7b0c
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-07-03 23:17:46 +0300

    Exercise

---
+++ Python OOP/1. First Steps/programmer.py
@@ -0,0 +1,20 @@
+class Programmer:
+    def __init__(self, name, language, skills):
+        self.name = name
+        self.language = language
+        self.skills = skills
+
+    def watch_course(self, course_name, language, skills_earned):
+        if language == self.language:
+            self.skills += skills_earned
+            return f"{self.name} watched {course_name}"
+        return f"{self.name} does not know {language}"
+
+    def change_language(self, new_language, skills_needed):
+        if new_language == self.language and skills_needed <= self.skills:
+            return f"{self.name} already knows {new_language}"
+        elif self.skills >= skills_needed:
+            previous_language = self.language
+            self.language = new_language
+            return f"{self.name} switched from {previous_language} to {new_language}"
+        return f"{self.name} needs {skills_needed - self.skills} more skills"
