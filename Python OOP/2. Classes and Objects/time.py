730244e9812e0f4a894d4cd874487acfb629b5a2
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-07-07 22:04:49 +0300

    Exercise

---
+++ Python OOP/2. Classes and Objects/time.py
@@ -0,0 +1,48 @@
+class Time:
+    max_hours = 23
+    max_minutes = 59
+    max_seconds = 59
+
+    def __init__(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def set_time(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def get_time(self):
+        hh = f"0{self.hours}" if self.hours < 10 else f"{self.hours}"
+        mm = f"0{self.minutes}" if self.minutes < 10 else f"{self.minutes}"
+        ss = f"0{self.seconds}" if self.seconds < 10 else f"{self.seconds}"
+        return f"{hh}:{mm}:{ss}"
+
+    def next_second(self):
+        self.seconds += 1
+        if self.seconds == 60:
+            self.seconds = 0
+            self.minutes += 1
+            if self.minutes == 60:
+                self.minutes = 0
+                self.hours += 1
+                if self.hours == 24:
+                    self.hours = 0
+        return self.get_time()
+
+
+
+
+
+
+
+
+time = Time(9, 30, 59)
+print(time.next_second())
+
+time = Time(10, 59, 59)
+print(time.next_second())
+
+time = Time(23, 59, 59)
+print(time.next_second())
