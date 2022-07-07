b53bd4d2078b3e285a565c19486963f2c15951f3
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-07-07 22:03:48 +0300

    Exercise

---
+++ Python OOP/2. Classes and Objects/glass.py
@@ -0,0 +1,19 @@
+class Glass:
+    capacity = 250
+
+    def __init__(self):
+        self.content = 0
+
+    def fill(self, ml):
+        if self.content + ml > self.capacity:
+            return f"Cannot add {ml} ml"
+        self.content += ml
+        return f"Glass filled with {ml} ml"
+
+    def empty(self):
+        self.content = 0
+        return "Glass is now empty"
+
+    def info(self):
+        space_left = self.capacity - self.content
+        return f"{space_left} ml left"
