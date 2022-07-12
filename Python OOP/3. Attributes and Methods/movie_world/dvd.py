2a412a08b36e9e85f5a6df59be990692e457db05
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-07-12 23:34:02 +0300

    Exercise

---
+++ Python OOP/3. Attributes and Methods/movie_world/dvd.py
@@ -0,0 +1,21 @@
+import calendar
+
+
+class DVD:
+    def __init__(self, name: str, id: int, creation_year: int, creation_month: str, age_restriction: int):
+        self.name = name
+        self.id = id
+        self.creation_year = creation_year
+        self.creation_month = creation_month
+        self.age_restriction = age_restriction
+        self.is_rented = False
+
+    def __repr__(self):
+        return f"{self.id}: {self.name} ({self.creation_month} {self.creation_year})" \
+               f" has age restriction {self.age_restriction}. Status: {'rented' if self.is_rented else 'not rented'}"
+
+    @classmethod
+    def from_date(cls, id: int, name: str, date: str, age_restriction: int):
+        month, year = date.split('.')[1:]
+        month = calendar.month_name[int(month)]
+        return cls(name, id, int(year), month, age_restriction)
