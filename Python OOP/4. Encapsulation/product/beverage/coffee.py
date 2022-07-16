cc6ad896864cf1f9c54b0d1a62762f59697f08ee
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-07-16 23:44:28 +0300

    Exercise

---
+++ Python OOP/4. Encapsulation/product/beverage/coffee.py
@@ -0,0 +1,18 @@
+from project.beverage.hot_beverage import HotBeverage
+
+
+class Coffee(HotBeverage):
+    MILLILITERS = 50
+    PRICE = 3.50
+
+    def __init__(self, name, caffeine, price=PRICE, milliliters=MILLILITERS, ):
+        super().__init__(name, price, milliliters)
+        self.caffeine = caffeine
+
+    @property
+    def caffeine(self):
+        return self.__caffeine
+
+    @caffeine.setter
+    def caffeine(self, value):
+        self.__caffeine = value
