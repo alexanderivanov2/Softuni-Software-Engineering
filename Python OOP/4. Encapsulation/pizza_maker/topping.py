6eaf16aea9914bbc63133858383a1c5024679a75
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-07-18 22:31:50 +0300

    Exercise

---
+++ Python OOP/4. Encapsulation/pizza_maker/topping.py
@@ -0,0 +1,26 @@
+class Topping:
+    def __init__(self, topping_type, weight):
+        self.topping_type = topping_type
+        self.weight = weight
+
+    @property
+    def topping_type(self):
+        return self.__topping_type
+
+    @topping_type.setter
+    def topping_type(self, topping_type):
+        if topping_type:
+            self.__topping_type = topping_type
+        else:
+            raise ValueError("The topping type cannot be an empty string")
+
+    @property
+    def weight(self):
+        return self.__weight
+
+    @weight.setter
+    def weight(self, weight):
+        if weight <= 0:
+            raise ValueError("The weight cannot be less or equal to zero")
+        self.__weight = weight
+
