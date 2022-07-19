e7f20757f5fbdc5ddcc2d0b85ccd5f9175fc9671
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-07-19 23:55:21 +0300

    Exercise

---
+++ Python OOP/4. Encapsulation/wild_zoo/cheetah.py
@@ -0,0 +1,6 @@
+from project_wild_zoo.animal import Animal
+
+
+class Cheetah(Animal):
+    def __init__(self, name, gender, age, money_for_care=60):
+        super().__init__(name, gender, age, money_for_care)
