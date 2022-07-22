88d57705bec2a9b88e4e0c853bdd86cbd3aad0a3
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-07-22 22:22:55 +0300

    Exercise

---
+++ Python OOP/5. Inheritance/hierarchical_inheritance_2/cat.py
@@ -0,0 +1,7 @@
+from project_hierarchical_inheritance_2.animal import Animal
+
+
+class Cat(Animal):
+    @staticmethod
+    def meow():
+        return "meowing..."
---
+++ Python OOP/5. Inheritance/hierarchical_inheritance_2/dog.py
@@ -0,0 +1,7 @@
+from project_hierarchical_inheritance_2.animal import Animal
+
+
+class Dog(Animal):
+    @staticmethod
+    def bark():
+        return "barking..."
