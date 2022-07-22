787bb28dfc03deac0a367394175546ed4ba21c44
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-07-22 22:25:13 +0300

    Exercise

---
+++ Python OOP/5. Inheritance/multilevel_inheritance_2/car.py
@@ -0,0 +1,7 @@
+from project_multilevel_inheritance_2.vehicle import Vehicle
+
+
+class Car(Vehicle):
+    @staticmethod
+    def drive():
+        return "driving..."
---
+++ Python OOP/5. Inheritance/multilevel_inheritance_2/sports_car.py
@@ -0,0 +1,7 @@
+from project_multilevel_inheritance_2.car import Car
+
+
+class SportsCar(Car):
+    @staticmethod
+    def race():
+        return "racing..."
