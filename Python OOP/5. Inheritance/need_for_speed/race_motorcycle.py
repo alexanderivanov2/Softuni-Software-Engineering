fa5f6365040c503503f9e66b4b479f28f6f276bd
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-07-23 23:13:55 +0300

    Exercise

---
+++ Python OOP/5. Inheritance/need_for_speed/race_motorcycle.py
@@ -0,0 +1,11 @@
+from project_need_for_speed.motorcycle import Motorcycle
+
+
+class RaceMotorcycle(Motorcycle):
+    DEFAULT_FUEL_CONSUMPTION = 8
+
+    def __init__(self, fuel, horse_power):
+        super().__init__(fuel, horse_power)
+        self.fuel_consumption = RaceMotorcycle.DEFAULT_FUEL_CONSUMPTION
+
+
