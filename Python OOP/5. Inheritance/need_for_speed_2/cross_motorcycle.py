860b2862566a9fab0f2209b71d0a05781de73804
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-07-23 23:15:05 +0300

    Exercise

---
+++ Python OOP/5. Inheritance/need_for_speed_2/cross_motorcycle.py
@@ -0,0 +1,6 @@
+from project_need_for_speed_2.motorcycle import Motorcycle
+
+
+class CrossMotorcycle(Motorcycle):
+    def __init__(self, fuel, horse_power):
+        super().__init__(fuel, horse_power)
