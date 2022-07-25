e5d2860c54c3fd1f01ad9d07e323db558ef16b6b
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-07-25 23:24:47 +0300

    Exercise

---
+++ Python OOP/5. Inheritance/players_and_monsters/blade_knight.py
@@ -0,0 +1,6 @@
+from project_players_and_monsters.dark_knight import DarkKnight
+
+
+class BladeKnight(DarkKnight):
+    def __init__(self, username, level):
+        super().__init__(username, level)
