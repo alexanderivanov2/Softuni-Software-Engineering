c5f924cab7ef3bfe5128fb8b612bb9794a04bb0e
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-08-05 23:29:42 +0300

    Exercise

---
+++ Python OOP/8. Iterators and Generators/example_yield.py
@@ -0,0 +1,15 @@
+def my_gen():
+    n = 1
+    print('This is printed first')
+    yield n
+
+    n += 1
+    print('This is printed second')
+    yield n
+
+    n += 1
+    print('This is printed at last')
+    yield n
+
+my_gen()
+
