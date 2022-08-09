ba596b6cd42ae2f6561c9c3b6cbb19303aff1efa
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-08-09 23:59:06 +0300

    Exercise

---
+++ Python OOP/9. Decorators/even_numbers.py
@@ -0,0 +1,13 @@
+def even_numbers(function):
+    def wrapper(*args):
+        result = function(*args)
+        evens = [x for x in result if x % 2 == 0]
+        return evens
+
+    return wrapper
+
+
+@even_numbers
+def get_numbers(numbers):
+    return numbers
+print(get_numbers([1, 2, 3, 4, 5]))
