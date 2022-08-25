6679bc2fdcbeafb29c7931464e41f14223c8a550
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-08-25 22:56:29 +0300

    Exercise

---
+++ JavaScript Fundamentals/JavaScript Fundamentals Data Types/requiredReading.js
@@ -0,0 +1,10 @@
+function timeToReadBookADay(pages, pagePerHour, days){
+    console.log((pages/ pagePerHour) / days);
+}
+
+timeToReadBookADay(212,
+    20 ,
+    2 );
+timeToReadBookADay(432,
+    15 ,
+    4 );
