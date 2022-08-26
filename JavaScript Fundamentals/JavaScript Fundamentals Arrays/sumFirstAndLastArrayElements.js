2e411eb8b17d285974b9c66b53450abc1b096880
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-08-26 23:25:08 +0300

    Exercise

---
+++ JavaScript Fundamentals/JavaScript Fundamentals Arrays/sumFirstAndLastArrayElements.js
@@ -0,0 +1,7 @@
+function sumFirstAndLastElements(arr){
+    let firstElement = arr[0];
+    let lastElement = arr[arr.length - 1];
+    console.log(firstElement + lastElement);
+}
+
+sumFirstAndLastElements([20, 30, 40]);
