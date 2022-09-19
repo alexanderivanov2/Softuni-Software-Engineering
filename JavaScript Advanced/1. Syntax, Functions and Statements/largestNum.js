e9a82e6dbe5760f369045a9b4785b3f3a0f3733b
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-09-19 20:04:53 +0300

    Exercise

---
+++ JavaScript Advanced/1. Syntax, Functions and Statements/largestNum.js
@@ -0,0 +1,7 @@
+function getLargestNum(num1, num2, num3) {
+    let largestNum = Math.max(num1, num2, num3);
+    console.log(`The largest number is ${largestNum}.`);
+}
+
+getLargestNum(5, -3, 16);
+getLargestNum(-3, -5, -22.5);
