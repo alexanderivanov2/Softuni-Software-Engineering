b30ff41ef3c264be7db13cd43efbb5254f472bf0
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-09-23 22:33:58 +0300

    Exercise

---
+++ JavaScript Advanced/2. Arrays and Nested Arrays/printEveryNthElementFromAnArray.js
@@ -0,0 +1,16 @@
+function printNthElementsOfArray(arr, step) {
+    let newArr = [];
+
+    for(let i = 0; i < arr.length; i+= step) {
+        newArr.push(arr[i]);
+    }
+
+    return newArr
+}
+
+console.log(printNthElementsOfArray(['5', 
+'20', 
+'31', 
+'4', 
+'20'], 
+2));
