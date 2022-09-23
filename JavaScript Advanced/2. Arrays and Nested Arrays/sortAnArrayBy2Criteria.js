5c37bf45b06a5561e10fb82443ad07787f451124
Alexander Ivanov <79406280+alexanderivanov2@users.noreply.github.com>
2022-09-23 22:34:21 +0300

    Exercise

---
+++ JavaScript Advanced/2. Arrays and Nested Arrays/sortAnArrayBy2Criteria.js
@@ -0,0 +1,24 @@
+function sortByTwoCriterias(arr) {
+    arr.sort((a, b) =>{
+        if (a.length != b.length) {
+            return a.length - b.length
+        } else {
+            return a.localeCompare(b)
+        }
+    });
+
+    console.log(arr.join('\n'));
+}
+
+sortByTwoCriterias(['alpha', 
+'beta', 
+'gamma']);
+sortByTwoCriterias(['Isacc', 
+'Theodor', 
+'Jack', 
+'Harrison', 
+'George']);
+sortByTwoCriterias(['test', 
+'Deny', 
+'omen', 
+'Default']);
