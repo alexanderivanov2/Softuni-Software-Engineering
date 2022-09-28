1d94d79ec7f0f00962f16c63a022a64b2443cf1c
Alexander Ivanov <alexanderivanov2277@gmail.com>
2022-09-28 23:51:23 +0300

    Exercise

---
+++ JavaScript Advanced/6. Advanced Functions/lab/areaAndVolumeCalculator.js
@@ -0,0 +1,25 @@
+function area() {
+    return Math.abs(this.x * this.y);
+};
+
+function vol() {
+    return Math.abs(this.x * this.y * this.z);
+};
+
+function solve(area, vol, input) {
+    
+    input = JSON.parse(input)
+    
+    let calc = input.map(el => {
+        return {area: area.call(el), volume: vol.call(el)};
+    });
+
+    return calc;
+}
+
+
+console.log(solve(area, vol, `[
+    {"x":"1","y":"2","z":"10"},
+    {"x":"7","y":"7","z":"10"},
+    {"x":"5","y":"2","z":"10"}
+    ]`));
