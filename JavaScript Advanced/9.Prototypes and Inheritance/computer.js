ccee49fe156efbe66df7207deac8457bbabbaea7
Alexander Ivanov <alexanderivanov2277@gmail.com>
2022-09-30 16:53:24 +0300

    Exercise

---
+++ JavaScript Advanced/9.Prototypes and Inheritance/computer.js
@@ -0,0 +1,116 @@
+function createComputerHierarchy() {
+    class Manufacturer {
+        constructor(manufacturer) {
+            this.manufacturer = manufacturer;
+        }
+    }
+
+    class Keyboard extends Manufacturer {
+        constructor(manufacturer, responseTime) {
+            super(manufacturer);
+            this.responseTime = responseTime;
+        }
+    }
+
+    class Monitor extends Manufacturer {
+        constructor(manufacturer, width, height) {
+            super(manufacturer);
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    class Battery extends Manufacturer {
+        constructor(manufacturer, expectedLife) {
+            super(manufacturer);
+            this.expectedLife = expectedLife;
+        }
+    }
+
+    
+    class Computer extends Manufacturer{
+        constructor(manufacturer, processorSpeed, ram, hardDiskSpace) {
+            if (new.target === Computer) {
+                throw new Error('Abstract classes cannot be instantiated!');
+            }
+            super(manufacturer);
+            this.processorSpeed = processorSpeed;
+            this.ram = ram;
+            this.hardDiskSpace = hardDiskSpace;
+        }
+    }
+
+    class Laptop extends Computer {
+        constructor(manufacturer, processorSpeed, ram, hardDiskSpace, weight, color, battery) {
+            super(manufacturer, processorSpeed, ram, hardDiskSpace);
+            this.weight = weight;
+            this.color = color;
+            this.battery = battery;
+        }
+
+        get battery() {
+            return this._battery;
+        }
+
+        set battery(value) {
+            if (!(value instanceof Battery)) {
+                throw new TypeError(`${value.constructor.name} is not a correct instance for the ${this.constructor.name} class!`);
+            }
+            this._battery = value;
+        }
+    }
+
+
+    class Desktop extends Computer {
+        constructor(manufacturer, processorSpeed, ram, hardDiskSpace, keyboard, monitor) {
+            super(manufacturer, processorSpeed, ram, hardDiskSpace);
+            this.keyboard = keyboard;
+            this.monitor = monitor;
+        }
+
+        get keyboard() {
+            return this._keyboard;
+        }
+
+        set keyboard(value) {
+            if (!(value instanceof Keyboard)) {
+                throw new TypeError(`${value.constructor.name} is not a correct instance for the ${this.constructor.name} class!`);
+            }
+            this._keyboard = value;
+        }
+
+        get monitor() {
+            return this._monitor;
+        }
+
+        set monitor(value) {
+            if(!(value instanceof Monitor)) {
+                throw new TypeError(`${value.constructor.name} is not a correct instance for the ${this.constructor.name} class!`);
+            }
+            this._monitor = value;
+        }
+    }
+
+    return {
+        Battery,
+        Keyboard,
+        Monitor,
+        Computer,
+        Laptop,
+        Desktop
+    }
+}
+
+
+let classes = createComputerHierarchy();
+let Computer = classes.Computer;
+let Laptop = classes.Laptop;
+let Desktop = classes.Desktop;
+let Monitor = classes.Monitor;
+let Battery = classes.Battery;
+let Keyboard = classes.Keyboard;
+
+let battery = new Battery('Energy', 3);
+console.log(battery);
+let laptop = new Laptop("Hewlett Packard", 2.4, 4, 0.5, 3.12, "Silver", battery);
+console.log(laptop);
---
+++ JavaScript Advanced/9.Prototypes and Inheritance/demo.js
@@ -0,0 +1,8 @@
+let arr = [1, 2, 3];
+let obj = {
+    'name': 'A2',
+    age:78
+}
+
+console.log(Object.getOwnPropertyDescriptors(obj));
+console.log(Object.getOwnPropertyDescriptors(arr));
